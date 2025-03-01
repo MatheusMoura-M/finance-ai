75877f12615f50fa2b537fe221063d8576551748
Matheus <matheus150101miranda@gmail.com>
2025-02-28 22:25:05 -0300

    chore: clerk setup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,17 @@
 import { LogInIcon } from "lucide-react";
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const LoginPage = () => {
+  const { userId } = auth();
+
+  if (userId) {
+    redirect("/");
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
@@ -22,12 +31,12 @@ const LoginPage = () => {
           facilitando o controle do seu orçamento.
         </p>
 
-        {/* <SignInButton> */}
-        <Button variant={"outline"}>
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
-        {/* </SignInButton> */}
+        <SignInButton>
+          <Button variant={"outline"}>
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
 
       <div className="relative h-full w-full">
