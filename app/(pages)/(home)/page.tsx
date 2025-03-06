6e2576f0aba801182ebede61da758594fd8e9655
Matheus <matheus150101miranda@gmail.com>
2025-03-05 22:04:19 -0300

    feat: add summary cards

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,13 +1,44 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "@/app/_components/navbar";
+import TimeSelect from "./_components/time-select";
+import SummaryCards from "./_components/summary-cards";
+import { isMatch } from "date-fns";
 
-export default function Home() {
+interface HomeProps {
+  searchParams: {
+    month: string;
+  };
+}
+
+const Home = async ({ searchParams: { month } }: HomeProps) => {
   const { userId } = auth();
 
   if (!userId) {
     redirect("/login");
   }
 
-  return <Navbar />;
-}
+  const monthIsValid = !month || !isMatch(month, "MM");
+
+  if (monthIsValid) {
+    redirect(`/?month=${month}`);
+  }
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="space-y-6 p-6">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+
+          <TimeSelect />
+        </div>
+
+        <SummaryCards month={month} />
+      </div>
+    </>
+  );
+};
+
+export default Home;
