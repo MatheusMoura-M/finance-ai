b1244dfcd799f4779076ab1236ced5eb468ef2f7
Matheus <matheus150101miranda@gmail.com>
2025-03-14 17:03:03 -0300

    refactor: ensure decimal warning does not happen

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -102,6 +102,6 @@ export const getDashboard = async (month: string) => {
     investmentsTotal,
     typesPercentage,
     totalExpensePerCategory,
-    lastTransactions,
+    lastTransactions: JSON.parse(JSON.stringify(lastTransactions)),
   };
 };
