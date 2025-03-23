3372c3e5662ff850fc4c4cb8fb80407ba3ae6126
Matheus <matheus150101miranda@gmail.com>
2025-03-22 21:31:39 -0300

    feat: test add stripe

--- app/(pages)/subscription/_actions/create-stripe-checkout/index.ts
+++ app/(pages)/subscription/_actions/create-stripe-checkout/index.ts
@@ -1,41 +1,40 @@
 "use server";
 
-// import { auth } from "@clerk/nextjs/server";
-// import Stripe from "stripe";
+import { auth } from "@clerk/nextjs/server";
+import Stripe from "stripe";
 
 export const createStripeCheckout = async () => {
-  // const { userId } = auth();
+  const { userId } = auth();
 
-  // if (!userId) {
-  //   throw new Error("Unauthorized");
-  // }
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
 
-  // if (!process.env.STRIPE_SECRET_KEY) {
-  //   throw new Error("Stripe secret key not found");
-  // }
+  if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("Stripe secret key not found");
+  }
 
-  // const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  //   apiVersion: "2024-10-28.acacia",
-  // });
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2024-10-28.acacia",
+  });
 
-  // const session = await stripe.checkout.sessions.create({
-  //   payment_method_types: ["card"],
-  //   mode: "subscription",
-  //   success_url: process.env.APP_URL,
-  //   cancel_url: process.env.APP_URL,
-  //   subscription_data: {
-  //     metadata: {
-  //       clerk_user_id: userId,
-  //     },
-  //   },
-  //   line_items: [
-  //     {
-  //       price: process.env.STRIPE_PREMIUM_PLAN_PRICE_ID,
-  //       quantity: 1,
-  //     },
-  //   ],
-  // });
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "subscription",
+    success_url: process.env.APP_URL,
+    cancel_url: process.env.APP_URL,
+    subscription_data: {
+      metadata: {
+        clerk_user_id: userId,
+      },
+    },
+    line_items: [
+      {
+        price: process.env.STRIPE_PREMIUM_PLAN_PRICE_ID,
+        quantity: 1,
+      },
+    ],
+  });
 
-  return { sessionId: "" };
-  // return { sessionId: session.id };
+  return { sessionId: session.id };
 };
