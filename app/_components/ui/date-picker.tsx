949275c83995a487222dd8070bf21f8fe46aded6
Matheus <matheus150101miranda@gmail.com>
2026-04-03 20:09:47 -0300

    fix: test bug to ios

--- app/_components/ui/date-picker.tsx
+++ app/_components/ui/date-picker.tsx
@@ -18,15 +18,17 @@ interface DatePickerProps {
 export const DatePicker = ({ value, onChange }: DatePickerProps) => {
   return (
     <Popover>
-      <PopoverTrigger asChild>
+      <PopoverTrigger>
         <Button
-          variant={"outline"}
+          type="button"
+          variant="outline"
           className={cn(
             "w-full justify-start text-left font-normal",
             !value && "text-muted-foreground",
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
+
           {value ? (
             new Date(value).toLocaleDateString("pt-BR", {
               day: "2-digit",
@@ -38,6 +40,7 @@ export const DatePicker = ({ value, onChange }: DatePickerProps) => {
           )}
         </Button>
       </PopoverTrigger>
+
       <PopoverContent className="w-auto p-0">
         <Calendar
           mode="single"
